37e08ee87a582bdb838db651389c1b0ec06e2a0b
iliasFs <110726288+iliasFs@users.noreply.github.com>
2023-04-05 16:47:14 +0300

    Write rename append read

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
 const fs = require("fs");
+const path = require("path");
 
-fs.readFile("files/starter.txt", "utf8", (err, data) => {
-  if (err) throw err;
+// fs.readFile("files/starter.txt", "utf8", (err, data) => {
+//   if (err) throw err;
 
-  //it will read Buffer. so we need to convert it to string
-  console.log(data);
-  //so the right way is
-  //console.log(data.toString());
+//   //it will read Buffer. so we need to convert it to string
+//   console.log(data);
+//   //so the right way is
+//   //console.log(data.toString());
 
-  //OR we can add another argument in the .readFile the utf8 and we dont need data.toString().
+//   //OR we can add another argument in the .readFile the utf8(witch defines the encoding) and we dont need data.toString().
+// });
+
+//exit on uncaught errors
+process.on("uncaughtExeption", (err) => {
+  console.error(`There was an ancaught error: ${err}`);
+
+  process.exit(1);
 });
+
+//instead of hardoding the path "files/starter.txt" there is a better way.
+
+fs.readFile(
+  path.join(__dirname, "files", "starter.txt"),
+  "utf8",
+  (err, data) => {
+    if (err) throw err;
+
+    console.log(data);
+  }
+);
+
+//WRITE FILE and fighting with asynchronicity
+
+// fs.writeFile(
+//   path.join(__dirname, "files", "reply.txt"),
+//   "Nice to meet you",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("Write Complete!");
+
+//     fs.appendFile(
+//       path.join(__dirname, "files", "reply.txt"),
+//       "\n\n Yes it is ",
+//       (err) => {
+//         if (err) throw err;
+//         console.log("Append Complete!");
+//         fs.rename(
+//           path.join(__dirname, "files", "reply.txt"),
+//           "newReply",
+//           (err) => {
+//             if (err) throw err;
+//             console.log("Rename Complete!");
+//           }
+//         );
+//       }
+//     );
+//   }
+// );
+
+
+
